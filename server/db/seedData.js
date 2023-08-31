16252bed331e8008c37a4e72bdda77a49ae83853
Sofie Christensen <sofiec510@gmail.com>
2023-08-31 13:33:57 -0400

    more data

---
+++ server/db/seedData.js
@@ -0,0 +1,20 @@
+const users = [
+    { username: 'sofiecodes', password: ilovemycat, name: 'Sofie Christensen' },
+    { username: 'bennieboi', password: ilovebroccoli, name: 'Bennie Tchop' }
+]
+
+const characters = [
+    { name: 'Greg', human: true, description: 'Younger brother of Wirt' },
+    { name: 'Wirt', human: true, description: 'Older brother of Greg' },
+    { name: 'The Woodsman', human: true, description: 'At the beck and call of The Beast' },
+    { name: 'The Beast', human: false, description: 'Uses the Woodsman to keep his soul alive' },
+    { name: 'Beatrice', human: false, description: 'Bluebird, a friend to Greg and Wirt' },
+    { name: 'The Frog', human: false, description: 'Pet of Greg, given many different names' },
+    { name: 'Adelaide', human: false, description: 'Witch in the forest' },
+    { name: 'Enoch', human: false, description: 'Pumpkinhead' },
+    { name: 'Sara', human: true, description: 'Girl that Wirt has a crush on' },
+    { name: 'Jason Funderberker', human: true, description: 'Boy that Wirt hates' },
+    { name: 'Miss Langtree', human: true, description: 'Teacher of many woodland animals' },
+    { name: 'Quincy Endicott', human: true, description: 'Rich man who believes his mansion is haunted' },
+    { name: 'Margueritte Grey', human: true, description: 'Rich neighbor of Quincy' },
+]
