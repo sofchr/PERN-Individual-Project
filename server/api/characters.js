fac7c6e82b71d395c3e965377446dfa0f3079ac2
Sofie Christensen <sofiec510@gmail.com>
2023-09-03 17:09:25 -0400

    character POST attempt, still getting error

--- server/api/characters.js
+++ server/api/characters.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllCharacters, getCharacterById } = require('../db/helpers/characters');
+const { getAllCharacters, getCharacterById, createCharacter } = require('../db/helpers/characters');
 
 // GET - /api/characters, get all characters
 router.get('/', async (req, res, next) => {
@@ -13,9 +13,20 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+//GET individual character
 router.get('/:characterId', async (req, res, next) => {
     try {
         const character = await getCharacterById(req.params.characterId);
+        res.send(character);
+    } catch (error) {
+        next(error);
+    }
+});
+
+//POST new character
+router.post('/', async (req, res, next) => {
+    try {
+        const character = await createCharacter(req.body);
         res.send(character);
     } catch (error) {
         next(error);
