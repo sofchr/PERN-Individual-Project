0a32bb012007fb72560123a2ae07ca1b4250674a
Sofie Christensen <sofiec510@gmail.com>
2023-09-06 22:16:22 -0400

    Posts and Characters routes working/rendering all

--- client/components/characters.jsx
+++ client/components/characters.jsx
@@ -1,26 +1,31 @@
 // import { useNavigate } from "react-router-dom";
-import { fetchAllCharacters } from "../../fetching";
+import { useState, useEffect } from "react";
+import { fetchAllCharacters } from "../fetching";
 
 export default function Characters() {
+  const [characters, setCharacters] = useState([]);
+
   useEffect(() => {
-    async function Characters() {
-      const characters = await fetchAllCharacters;
+    async function getAllCharacters() {
+      const characters = await fetchAllCharacters();
+      setCharacters(characters);
+      console.log(characters);
+      return characters;
     }
-  });
+    getAllCharacters();
+  }, []);
 
   return (
-    <h1>
+    <div className="characters-container">
       {characters.map((character) => {
         return (
-          <>
-            <div key={character.characterId}>
-              <h2>{character.name}</h2>
-              <p>{character.description}</p>
-              <img src={character.image} alt={character.name} />
-            </div>
-          </>
+          <div className="single-character" key={character.characterId}>
+            <p>{character.name}</p>
+            <p>{character.description}</p>
+            <img src={character.image} alt={character.name} />
+          </div>
         );
       })}
-    </h1>
+    </div>
   );
 }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-// import Characters from "./components/characters";
+import { Routes, Route } from "react-router-dom";
+import Characters from "../components/Characters";
 import NavBar from "../components/NavBar";
 import Posts from "../components/Posts";
 import Home from "../components/Home";
@@ -14,7 +14,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={Home()} />
-        {/* <Route path="/characters" element={Characters()} /> */}
+        <Route path="/characters" element={Characters()} />
         <Route path="/posts" element={Posts()} />
       </Routes>
     </>
