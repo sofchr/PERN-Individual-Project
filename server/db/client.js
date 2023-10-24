9b6cbbe60f6f32aff8ed31a3b4f358768315ed32
Sofie Christensen <sofiec510@gmail.com>
2023-10-24 17:19:15 -0400

    client edit

--- server/db/client.js
+++ server/db/client.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg')
 const gardenwall = 'gardenwall'
 // const client = new Client(`postgres://localhost:5432/${gardenwall}`)
-const client = newClient(`postgres://gardenwall_user:tYmleUxlzznKAC8YwAVEkOsR0Bxuu8K4@dpg-cks339unfb1c73f0l31g-a/gardenwall`)
+const client = new Client(`postgres://gardenwall_user:tYmleUxlzznKAC8YwAVEkOsR0Bxuu8K4@dpg-cks339unfb1c73f0l31g-a/gardenwall`)
 
 
 module.exports = client
