84cee046de27cce1858c1d504fde45a9a31bacd6
Sofie Christensen <sofiec510@gmail.com>
2023-11-29 17:15:41 -0500

    new deployment branch

--- server/db/client.js
+++ server/db/client.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg')
-const gardenwall = 'gardenwall'
-const client = new Client(`postgres://localhost:5432/${gardenwall}`)
-
+const gardenwall = 'gardenwall';
+// const client = new Client(`postgres://localhost:5432/${gardenwall}`)
+const client = new Client('postgres://gardenwall_rwhz_user:RHfyPALJ6VRQzrk1Ntp3q69nTtFQG5hF@dpg-cljre2hll56s73bonlf0-a/gardenwall_rwhz')
 
 
 module.exports = client
