67f2b46661669c273c5a448513a106132cf614fc
Sofie Christensen <sofiec510@gmail.com>
2023-11-30 16:27:31 -0500

    file path edits for deployment

--- client/src/components/Characters.jsx
+++ client/src/components/Characters.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { fetchAllCharacters } from "../fetching";
+import { fetchAllCharacters } from "../../fetching";
 import { useNavigate } from "react-router-dom";
 
 export default function Characters() {
