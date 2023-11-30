2a692902235d4556a3731c42ae3af3d5dab30c51
Sofie Christensen <sofiec510@gmail.com>
2023-11-29 21:44:52 -0500

    new internal db link

--- server/db/client.js
+++ server/db/client.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg')
 const gardenwall = 'gardenwall';
 // const client = new Client(`postgres://localhost:5432/${gardenwall}`)
-const client = new Client('postgres://gardenwall_a5bp_user:tH0orIZcDM18jKyogX9Kh5UzxGSVctt3@dpg-cljt1gdae00c73frhjtg-a/gardenwall_a5bp')
+const client = new Client('postgres://gardenwall_wegj_user:jwOBVN3abukssHVmKcjBf7pgaGSHB0Ph@dpg-cljvdg6g1b2c739ekgkg-a/gardenwall_wegj')
 
 
 module.exports = client
