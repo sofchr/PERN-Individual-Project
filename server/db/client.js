3fd4e47743b3406ee22c4452762d65125c4310dc
Sofie Christensen <sofiec510@gmail.com>
2023-11-29 20:52:59 -0500

    db

--- server/db/client.js
+++ server/db/client.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg')
-const dbname = 'gardenwall';
+const gardenwall = 'gardenwall';
 // const client = new Client(`postgres://localhost:5432/${gardenwall}`)
 const client = new Client('postgres://gardenwall_a5bp_user:tH0orIZcDM18jKyogX9Kh5UzxGSVctt3@dpg-cljt1gdae00c73frhjtg-a/gardenwall_a5bp')
 
